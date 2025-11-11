0afbba24b2f64260681ef8d2a94653c9ec8673a0
leodymann <hleodymann@gmail.com>
2025-11-11 09:20:27 -0300

    crud complete

--- app/crud.py
+++ app/crud.py
@@ -3,27 +3,53 @@ import string
 from sqlalchemy.ext.asyncio import AsyncSession
 from sqlalchemy.future import select
 from sqlalchemy.exc import IntegrityError
+from fastapi import HTTPException, status
 
 from app.models import URL
 from app.schemas import URLCreate
 
+
 def generate_hash(length: int = 6) -> str:
-    # função de gerar o hash da URL
+    # generate hash 6 digits
     return ''.join(random.choices(string.ascii_letters + string.digits, k=length))
 
+
 async def create_url(db: AsyncSession, url: URLCreate) -> URL:
-    # função para criar URLs
-    short_hash = generate_hash() # geração de hash (6 caracteres)
-    new_url = URL(original_url=url.original_url, short_hash=short_hash, user_id=url.user_id) # url original + nova com hash
-    db.add(new_url) # envio a adição à sessão
+    # create url + hash
+    short_hash = generate_hash()
+    new_url = URL(original_url=url.original_url, short_hash=short_hash, user_id=url.user_id)
+    db.add(new_url)
     try:
-        await db.commit() # envio ao database
-        await db.refresh(new_url) # update no database
-        return new_url # retorno o obj criado
+        await db.commit()
+        await db.refresh(new_url)
+        return new_url
     except IntegrityError:
         await db.rollback()
         return await create_url(db, url)  # colisão de hash
 
-async def get_url_by_hash(db: AsyncSession, short_hash: str): # query para achar a URL especifica
-    result = await db.execute(select(URL).where(URL.short_hash == short_hash)) # query assíncrona
-    return result.scalars().first() # primeiro da consulta
+
+async def get_url_by_hash(db: AsyncSession, short_hash: str):
+    # search url by hash
+    result = await db.execute(select(URL).where(URL.short_hash == short_hash))
+    return result.scalars().first()
+
+
+async def delete_url(db: AsyncSession, short_hash: str):
+    # delete url by hash
+    result = await db.execute(select(URL).where(URL.short_hash == short_hash))
+    url = result.scalars().first()
+
+    if not url:
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail="URL não encontrada")
+
+    await db.delete(url)
+    await db.commit()
+    return {"detail": "URL deletada com sucesso"}
+
+
+async def redirect_url(db: AsyncSession, short_hash: str):
+    # redirect url by hash
+    url = await get_url_by_hash(db, short_hash)
+    if not url:
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail="URL não encontrada")
+    return url.original_url
--- app/main.py
+++ app/main.py
@@ -1,33 +1,55 @@
 from fastapi import FastAPI, Depends, HTTPException
+from fastapi.responses import RedirectResponse
 from sqlalchemy.ext.asyncio import AsyncSession
-from app.database import get_db, engine, Base
+from app.database import get_db, Base, engine
 from app import crud, schemas
 from app.cache import cache
-import asyncio
 
 app = FastAPI(title="URL Shortener API")
 
+
 @app.on_event("startup")
-async def startup():
-    # create table:
+async def on_startup():
+    # create tables if they don't already exist.
     async with engine.begin() as conn:
         await conn.run_sync(Base.metadata.create_all)
 
-@app.post("/urls/", response_model=schemas.URLResponse)
-async def create_url(url: schemas.URLCreate, db: AsyncSession = Depends(get_db)):
+
+@app.post("/shorten/", response_model=schemas.URLResponse)
+async def create_short_url(url: schemas.URLCreate, db: AsyncSession = Depends(get_db)):
+    # create short url
+    cached_url = cache.get(url.original_url)
+    if cached_url:
+        return cached_url
+
     new_url = await crud.create_url(db, url)
-    cache.set(new_url.short_hash, new_url.original_url)
+    cache.set(url.original_url, new_url)
     return new_url
 
-@app.get("/urls/{short_hash}")
-async def get_url(short_hash: str, db: AsyncSession = Depends(get_db)):
+
+@app.get("/url/{short_hash}", response_model=schemas.URLResponse)
+async def get_short_url(short_hash: str, db: AsyncSession = Depends(get_db)):
+    # return info short url
     cached = cache.get(short_hash)
     if cached:
-        return {"original_url": cached, "cached": True}
+        return cached
 
-    url_obj = await crud.get_url_by_hash(db, short_hash)
-    if not url_obj:
-        raise HTTPException(status_code=404, detail="URL not found")
+    db_url = await crud.get_url_by_hash(db, short_hash)
+    if not db_url:
+        raise HTTPException(status_code=404, detail="URL não encontrada")
 
-    cache.set(short_hash, url_obj.original_url)
-    return {"original_url": url_obj.original_url, "cached": False}
+    cache.set(short_hash, db_url)
+    return db_url
+
+
+@app.get("/r/{short_hash}")
+async def redirect_short_url(short_hash: str, db: AsyncSession = Depends(get_db)):
+    # redirect url original
+    original_url = await crud.redirect_url(db, short_hash)
+    return RedirectResponse(url=original_url)
+
+
+@app.delete("/delete/{short_hash}")
+async def delete_short_url(short_hash: str, db: AsyncSession = Depends(get_db)):
+    # delete short url by hash
+    return await crud.delete_url(db, short_hash)
